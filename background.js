29577829703e051f3b8911c7afc95b9a06b3831d
Ed Williams <isometimesgit@users.noreply.github.com>
2018-11-26 00:52:01 +0000

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    (details) => {
+        const fid = details.url.match(/^https:\/\/openload\.co\/f\/([\w]+)\/.+$/)[1];
+        if (fid) {
+            return { redirectUrl: "https://openload.co/f/" + fid + "/" };
+        }
+    }, {
+        urls: [
+            "https://openload.co/f/*"
+        ],
+        types: ["main_frame"]
+    },
+    ["blocking"]
+);
